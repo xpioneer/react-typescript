8baa61b58f98aa5a5228a976f16926e3e049f1d7
xpioneer <1286283280@qq.com>
2018-11-07 21:01:43 +0800

    add axios

--- src/pages/demo/demo.tsx
+++ src/pages/demo/demo.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 
+declare const $http: any
+
 export class Demo extends React.Component {
+
+  componentDidMount() {
+    console.log($http)
+  }
+
   render(){
     return <div>
       <p>hello, world! <br/> this is demo page.</p>
---
+++ src/utils/http.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const $http = axios.create({
+  baseURL: '',
+  transformResponse: [function (data) {
+    if (typeof data === 'string') {
+      return JSON.parse(data)
+    }
+    return data
+  }],
+  timeout: 30000
+})
+
+$http.interceptors.request.use(config => {
+  // config.headers['SourceHead'] = 'H5'
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+$http.interceptors.response.use(response => {
+  return Promise.resolve(response.data)
+}, error => {
+  return Promise.reject(error.response)
+})
+
+export default $http
