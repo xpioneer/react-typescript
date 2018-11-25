bb4ef9f12d5c78a3dcefb42ec39885536b068c89
xpioneer <1286283280@qq.com>
2018-11-25 15:37:27 +0800

    toggle language, add localStorage

--- src/pages/home/store.ts
+++ src/pages/home/store.ts
@@ -1,19 +1,19 @@
 import { observable, action, autorun, runInAction } from 'mobx';
 import * as zh_CN from 'antd/lib/locale-provider/zh_CN'
-import { store, Storage } from '@utils/tools'
+import { store } from '@utils/tools'
 
 const zhCN: any = zh_CN
 
 class HomeStore {
   @observable collapsed: boolean = false
-  @observable lang: any
+  @observable lang: any = store.get('Lang') === 'CN' ? zhCN : undefined
   
   @action toggleMenu = (): void => {
     this.collapsed = !this.collapsed
   }
 
   @action toggleLang = (value: any) => {
-    console.log(store, Storage)
+    store.set('Lang', !value ? 'CN' : 'EN')
     this.lang = !value ? zhCN : undefined
   }
 
