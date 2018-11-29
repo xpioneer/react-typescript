9fb35b2409673e7505db4a11eef8d4801b02e3f9
xpioneer <1286283280@qq.com>
2018-11-29 10:51:05 +0800

    add webpackChunkName

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,11 +8,11 @@ const {lazy, Suspense} = React
 const Dashboard = lazy(() => import( /* webpackChunkName:"dashboard" */ '@pages/dashboard/dashboard'))
 const Chart = lazy(() => import( /* webpackChunkName:"charts" */ '@pages/charts'))
 const LogApi = lazy(() => import( /* webpackChunkName:"logApi" */ '@pages/logs/api'))
-const LogErrors = lazy(() => import('@pages/logs/errors'))
-const ArticleList = lazy(() => import('@pages/article/articleList'))
-const ArticleEdit = lazy(() => import('@pages/article/articleEdit'))
-const ArticleCreate = lazy(() => import('@pages/article/articleCreate'))
-const Demo = lazy(() => import('@pages/demo/demo'))
+const LogErrors = lazy(() => import( /* webpackChunkName:"logErrors" */ '@pages/logs/errors'))
+const ArticleList = lazy(() => import( /* webpackChunkName:"articleList" */ '@pages/article/articleList'))
+const ArticleEdit = lazy(() => import( /* webpackChunkName:"articleEdit" */ '@pages/article/articleEdit'))
+const ArticleCreate = lazy(() => import( /* webpackChunkName:"articleCreate" */ '@pages/article/articleCreate'))
+const Demo = lazy(() => import( /* webpackChunkName:"demo" */ '@pages/demo/demo'))
 
 
 export const routes: RouteProps[] = [
