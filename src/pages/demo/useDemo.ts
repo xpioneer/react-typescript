9f5fa746f5528e9a819953d39ddf5c167051cdbe
xpioneer <1286283280@qq.com>
2024-08-14 13:10:05 +0800

    demo page: form useForm

--- src/pages/demo/useDemo.ts
+++ src/pages/demo/useDemo.ts
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { observable, action, autorun, runInAction } from 'mobx'
-import { message } from 'antd'
+import { message, UploadFile } from 'antd'
+import { APIFormTest } from 'types/demo'
 
 interface IParamsData {
   [key: string]: any
@@ -23,47 +23,23 @@ const defaultParams: IParamsData = {
 
 
 export const useDemoState = () => {
-  const [count, setCount] = useState(90)
-  const [apiUrl, setApiUrl] = useState( '/api')
-  const [apiType, setApiType] = useState( '/api')
-  const [apiMethod, setApiMethod] = useState( 'GET')
-  const [apiParams, setApiParams] = useState( '')
-  const [apiResult, setApiResult] = useState( '')
-  const [fileVal, setFileVal] = useState()
-
-  const add = useCallback(() => {
-    setCount(x => ++x) 
-  }, [])
-  const desc = useCallback(() => {
-    setCount(x => --x) 
-  }, [])
-
-  const inputApi = useCallback((value: string) => {
-    setApiUrl(value)
-  }, [])
+  const [response, setResponse] = useState( '')
 
   const typeChange = useCallback((type: string) => {
-    setApiType(type)
+    // setApiType(type)
     if (type === '/graphql') {
-      setApiParams(JSON.stringify(defaultParams, null, '    '))
+      // setApiParams(JSON.stringify(defaultParams, null, '    '))
     } else {
-      setApiParams('')
+      // setApiParams('')
     }
   }, [])
 
-  const methodChange = useCallback((type: string) => {
-    setApiMethod(type)
-  }, [])
-
-  const inputParams = useCallback((value: string) => {
-    setApiParams(value)
-  }, [])
-
-  const testApi = useCallback(() => {
+  const onRequest = useCallback((data: APIFormTest) => {
+    const { url, method, apiType, params } = data
     let jsonData = {}, error = null
     try {
-      if (apiParams) {
-        jsonData = JSON.parse(apiParams)
+      if (params) {
+        jsonData = JSON.parse(params)
       }
     } catch (e) {
       error = e
@@ -73,73 +49,40 @@ export const useDemoState = () => {
       return
     }
     if (apiType === '/api') {
-      if (apiMethod === 'GET') {
-        $http.get(apiUrl + fmtGetParams(jsonData)).then(res => {
-          setApiResult(JSON.stringify(res, null, '    '))
+      if (method === 'GET') {
+        $http.get(url + fmtGetParams(jsonData)).then(res => {
+          setResponse(JSON.stringify(res, null, '    '))
         })
-      } else if (apiMethod === 'POST') {
-        $http.post(apiUrl, jsonData).then(res => {
-          setApiResult(JSON.stringify(res, null, '    '))
+      } else {
+        $http[method](url, jsonData).then((res: any) => {
+          setResponse(JSON.stringify(res, null, '    '))
         })
-      } else if (apiMethod === 'PUT') {
-        $http.post(apiUrl, jsonData).then(res => {
-          setApiResult(JSON.stringify(res, null, '    '))
-        })
-      } else if (apiMethod === 'DELETE') {
-        $http.post(apiUrl, jsonData).then(res => {
-          setApiResult(JSON.stringify(res, null, '    '))
-        })
-      }
-      
+      } 
     } else {
-      $http.post(apiUrl, jsonData).then(res => {
-        setApiResult(JSON.stringify(res, null, '    '))
+      $http.post(url, jsonData).then(res => {
+        setResponse(JSON.stringify(res, null, '    '))
       }, err => {
-        setApiResult(JSON.stringify(err.data, null, '    '))
+        setResponse(JSON.stringify(err.data, null, '    '))
       })
     }
   }, [])
 
-  const fileChange = useCallback((e: any) => {
-    if (e && e.target.files) {
-      setFileVal(e.target.files[0])
-    } else {
-      setFileVal(undefined)
-    }
-  }, [])
-
-  const upload = useCallback(() => {
-    console.log(fileVal)
-    if (!fileVal) {
-      $msg.warn('请选择文件')
-      return
-    }
+  const upload = useCallback((file: File) => {
     const fd = new FormData()
-    fd.append('file', fileVal)
+    fd.append('file', file)
 
     $http.post('/api/upload', fd).then(res => {
       console.log(res)
     }, err => {
       console.log(err, 'err')
     })
-  }, [fileVal])
+  }, [])
 
 
   return {
-    count,
-    add,
-    desc,
-    inputApi,
-    methodChange,
     typeChange,
-    inputParams,
-    testApi,
-    apiUrl,
-    apiType,
-    apiMethod,
-    apiParams,
-    apiResult,
-    fileVal,
-    fileChange, upload
+    onRequest,
+    response,
+    upload
   }
 }
---
+++ src/types/demo.ts
@@ -0,0 +1,25 @@
+import { object2Options } from '@utils/tools'
+
+export interface APIFormTest {
+  url: string
+  method: string
+  apiType: string
+  params: string
+}
+
+
+export enum Method {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export const methodOpts = object2Options(Method)
+
+export enum APIType {
+  'RESTful' = '/api',
+  'Graphql' = '/graphql'
+}
+
+export const apiTypeOpts = object2Options(APIType)
