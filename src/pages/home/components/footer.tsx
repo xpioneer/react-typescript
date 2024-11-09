fbc8683db4777a2ae3f59450edf4c1e15e722e47
xpioneer <1286283280@qq.com>
2024-11-09 22:23:00 +0800

    footer padding sm

--- src/pages/home/components/footer.tsx
+++ src/pages/home/components/footer.tsx
@@ -4,8 +4,8 @@ import { Layout, Flex, theme } from 'antd'
 const { Footer } = Layout
 
 export default () => {
-  const { token: { boxShadow } } = theme.useToken()
-  return <Footer style={{boxShadow}}>
+  const { token: { boxShadow, paddingSM } } = theme.useToken()
+  return <Footer style={{boxShadow, padding: paddingSM}}>
     <Flex justify='center'>
       Copyright by&#160;
       <a target='_blank' href="https://github.com/xpioneer">xpioneer</a>
