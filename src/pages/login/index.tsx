89e7becc7c56d4eb67632453a9663f8820fea828
xpioneer <1286283280@qq.com>
2024-12-19 21:47:38 +0800

    code format

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,15 +4,13 @@ import { Form, Input, Button, Checkbox, Space, Flex } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import { onLogin } from '../../services/account'
 import { LoginForm } from '@/types/account'
-import { storage } from "@utils/tools"
-import { JWT_TOKEN, REDIRECT_URL } from "@constants/index"
+import { storage } from '@utils/tools'
+import { JWT_TOKEN, REDIRECT_URL } from '@constants/index'
 import { useAppStore, setAuthorized } from '@/stores'
 import { WaveComponent } from './wave'
 import styles from './style.module.scss'
 
-
 const LoginPage: React.FC = () => {
-
   const navigate = useNavigate()
 
   const [, dispatch] = useAppStore()
@@ -23,42 +21,43 @@ const LoginPage: React.FC = () => {
 
   const onFinish = (values: LoginForm) => {
     setLoading(true)
-    onLogin(values).then(res => {
-      storage.set(JWT_TOKEN, res) // store jwt token
-      dispatch(setAuthorized(true))
-      navigate(sessionStorage.getItem(REDIRECT_URL) || '/')
-    }).finally(() => setLoading(false))
+    onLogin(values)
+      .then((res) => {
+        storage.set(JWT_TOKEN, res) // store jwt token
+        dispatch(setAuthorized(true))
+        navigate(sessionStorage.getItem(REDIRECT_URL) || '/')
+      })
+      .finally(() => setLoading(false))
   }
 
-
-  return <div className={styles.login}>
-    <div className={styles.waveW}>
-      <WaveComponent />
-    </div>
-    <Form
-      className={styles.form}
-      form={form}
-      onFinish={onFinish}
-    >
-      <h2>Content Management System</h2>
-      <Form.Item name="username" rules={[{required: true}]}>
-        <Input prefix={<UserOutlined />} placeholder="Username"/>
-      </Form.Item>
-      <Form.Item name="password" rules={[{required: true}]}>
-        <Input prefix={<LockOutlined />} type="password" placeholder="Password"/>
-      </Form.Item>
-      <Flex justify="space-between" align="baseline">
-        <Form.Item name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
+  return (
+    <div className={styles.login}>
+      <div className={styles.waveW}>
+        <WaveComponent />
+      </div>
+      <Form className={styles.form} form={form} onFinish={onFinish}>
+        <h2>Content Management System</h2>
+        <Form.Item name="username" rules={[{ required: true }]}>
+          <Input prefix={<UserOutlined />} placeholder="Username" />
         </Form.Item>
-        <a href="">Forgot password</a>
-      </Flex>
-      <Form.Item>
-        <Button type="primary" htmlType='submit' loading={loading}>Log in</Button>
-      </Form.Item>
-      <a href="/register">Register now!</a>
-    </Form>
-  </div>
+        <Form.Item name="password" rules={[{ required: true }]}>
+          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Flex justify="space-between" align="baseline">
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <a href="">Forgot password</a>
+        </Flex>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Log in
+          </Button>
+        </Form.Item>
+        <a href="/register">Register now!</a>
+      </Form>
+    </div>
+  )
 }
 
 export default LoginPage
--- src/pages/login/wave.tsx
+++ src/pages/login/wave.tsx
@@ -1,65 +1,66 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-import { useAppStore } from '@/stores';
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import { useAppStore } from '@/stores'
 
-const Wave = ({color}: {color: THREE.Color}) => {
-
-  const meshRef = useRef<THREE.Mesh>(null);
-  const geometryRef = useRef<THREE.PlaneGeometry>(null);
+const Wave = ({ color }: { color: THREE.Color }) => {
+  const meshRef = useRef<THREE.Mesh>(null)
+  const geometryRef = useRef<THREE.PlaneGeometry>(null)
 
   // Wave parameters
   const waveParams = {
     amplitude: 2,
     wavelength: 10,
-    frequency: 0.2
-  };
+    frequency: 0.2,
+  }
 
   useFrame(({ clock }) => {
-    const time = clock.getElapsedTime();
-    const geometry = geometryRef.current!;
-    const positionAttribute = geometry.attributes.position;
-    
+    const time = clock.getElapsedTime()
+    const geometry = geometryRef.current!
+    const positionAttribute = geometry.attributes.position
+
     for (let i = 0; i < positionAttribute.count; i++) {
-      const x = positionAttribute.getX(i);
-      const y = positionAttribute.getY(i);
-      const z = Math.sin(x * waveParams.frequency + time) * waveParams.amplitude
-              + Math.cos(y * waveParams.frequency + time) * waveParams.amplitude;
-      positionAttribute.setZ(i, z);
+      const x = positionAttribute.getX(i)
+      const y = positionAttribute.getY(i)
+      const z =
+        Math.sin(x * waveParams.frequency + time) * waveParams.amplitude +
+        Math.cos(y * waveParams.frequency + time) * waveParams.amplitude
+      positionAttribute.setZ(i, z)
     }
 
-    positionAttribute.needsUpdate = true; // Notify Three.js that the vertices have changed
-  });
+    positionAttribute.needsUpdate = true // Notify Three.js that the vertices have changed
+  })
 
   return (
-    <mesh ref={meshRef} rotation={[-Math.PI/4, 0, 0]}>
+    <mesh ref={meshRef} rotation={[-Math.PI / 4, 0, 0]}>
       <planeGeometry ref={geometryRef} args={[800, 800, 500, 500]} />
       <meshPhongMaterial color={color} wireframe />
     </mesh>
-  );
-};
+  )
+}
 
 export const WaveComponent: React.FC = () => {
-
-  const [{colorPrimary}] = useAppStore()
+  const [{ colorPrimary }] = useAppStore()
 
   const color = new THREE.Color(colorPrimary)
-  const rgb = [color.r, color.g, color.b].map(i => Math.round(i * 255))
+  const rgb = [color.r, color.g, color.b].map((i) => Math.round(i * 255))
 
-  return <Canvas
-    gl={{
-      antialias: true,
-      alpha: true,
-    }}
-    style={{
-      backgroundColor: `rgba(${rgb.join()}, 0.05)`,
-      width: '100%',
-      height: '100%',
-    }}
-    camera={{ position: [0, 0, 60], near: 0.1, far: 1000 }}
-  >
-    <ambientLight intensity={0.5} />
-    <directionalLight position={[-100, -100, 1000]} intensity={1} />
-    <Wave color={color} />
-  </Canvas>
+  return (
+    <Canvas
+      gl={{
+        antialias: true,
+        alpha: true,
+      }}
+      style={{
+        backgroundColor: `rgba(${rgb.join()}, 0.05)`,
+        width: '100%',
+        height: '100%',
+      }}
+      camera={{ position: [0, 0, 60], near: 0.1, far: 1000 }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-100, -100, 1000]} intensity={1} />
+      <Wave color={color} />
+    </Canvas>
+  )
 }
